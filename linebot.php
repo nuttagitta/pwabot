977f51f9bdbff51c824f9d8183049666494869d6
nuttagitta <74849705+nuttagitta@users.noreply.github.com>
2020-11-22 17:54:14 +0700

    Update linebot.php

--- linebot.php
+++ linebot.php
@@ -1,56 +1,32 @@
-<?php
+const functions = require('firebase-functions');
+const request = require('request-promise');
 
+const LINE_MESSAGING_API = 'https://api.line.me/v2/bot/message';
+const LINE_HEADER = {
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer xxxxx`
+};
 
-$API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'Qv47j+C1M6b8avaEE30TmVfXixf2veNVHRHRa8OaQOukDQ0j9gfdYzPtlkypFZECVeWI0N0sRmHDwwbFSpVhEPQhjt9j3ddwduF2BT1r52P11umWun+/dmO+eA+6NyQMcVdKJEznrj4WLzsbgXRrXwdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = 'c03063939f68af706e840da2b1cbbca1';
+exports.LineBot = functions.https.onRequest((req, res) => {
+  if (req.body.events[0].message.type !== 'text') {
+    return;
+  }
+  reply(req.body);
+});
 
-
-$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
-
-$request = file_get_contents('php://input');   // Get request content
-$request_array = json_decode($request, true);   // Decode JSON to Array
-
-
-
-if ( sizeof($request_array['events']) > 0 ) {
-
-    foreach ($request_array['events'] as $event) {
-
-        $reply_message = '';
-        $reply_token = $event['replyToken'];
-
-
-        $data = [
-            'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
-        ];
-        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-        echo "Result: ".$send_result."\r\n";
-        
-    }
-}
-
-echo "OK";
-
-
-
-
-function send_reply_message($url, $post_header, $post_body)
-{
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-
-    return $result;
-}
-
-?>
+const reply = (bodyResponse) => {
+  return request({
+    method: `POST`,
+    uri: `${LINE_MESSAGING_API}/reply`,
+    headers: LINE_HEADER,
+    body: JSON.stringify({
+      replyToken: bodyResponse.events[0].replyToken,
+      messages: [
+        {
+          type: `text`,
+          text: bodyResponse.events[0].message.text
+        }
+	  ]
+    })
+  });
+};
